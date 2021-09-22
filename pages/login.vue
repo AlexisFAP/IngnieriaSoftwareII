ecd804e063f545ba60fd1ed61fd1354d13f4e156
AlexisFAP <alexispatinoagudelo@gmail.com>
2021-09-22 05:40:59 -0500

    add nuxt

---
+++ pages/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-card>
+      <v-form ref="formularioLogin">
+        <v-card-title primary-title> Log In </v-card-title>
+        <v-card-text>
+          <v-text-field
+            label="Identificación"
+            v-model="id"
+            :rules="requiredRule"
+          ></v-text-field>
+          <v-text-field
+            type="password"
+            label="Contraseña"
+            v-model="clave"
+            :rules="requiredRule"
+          ></v-text-field>
+        </v-card-text>
+        <v-progress-linear
+          :indeterminate="true"
+          v-if="loading"
+        ></v-progress-linear>
+        <v-card-actions>
+          <v-btn color="success">Log In</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </div>
+</template>
---
+++ pages/singup.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-card>
+      <v-form ref="formularioLogin">
+        <v-card-title primary-title> Sing Up </v-card-title>
+        <v-card-text>
+          <v-text-field
+            label="First Name"
+            v-model="firstname"
+            :rules="requiredRule"
+          ></v-text-field>
+          <v-text-field
+            label="Last Name"
+            v-model="lastname"
+            :rules="requiredRule"
+          ></v-text-field>
+          <v-text-field
+            label="Email"
+            v-model="email"
+            :rules="requiredRule"
+          ></v-text-field>
+          <v-text-field
+            type="password"
+            label="Contraseña"
+            v-model="clave"
+            :rules="requiredRule"
+          ></v-text-field>
+        </v-card-text>
+        <v-progress-linear
+          :indeterminate="true"
+          v-if="loading"
+        ></v-progress-linear>
+        <v-card-actions>
+          <v-btn color="success">Sing Up</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </div>
+</template>
