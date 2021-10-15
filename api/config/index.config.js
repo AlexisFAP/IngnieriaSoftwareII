efe70541f0db11c7e7e17c59b49539dfc6dacd3b
AlexisFAP <alexispatinoagudelo@gmail.com>
2021-10-14 19:23:59 -0500

    heroku desplegado

--- api/config/index.config.js
+++ api/config/index.config.js
@@ -6,7 +6,7 @@
 
 const PGUSER='fmibbnwoopmqia' 
 const PGHOST='ec2-54-166-37-125.compute-1.amazonaws.com'
-const PGPASSWORD='09091a73c421ea83e9cccf704f3bb676caef562d287c1676d0ba80aa27fb41eb  ' 
+const PGPASSWORD='09091a73c421ea83e9cccf704f3bb676caef562d287c1676d0ba80aa27fb41eb' 
 const PGDATABASE='d6pipv794hgjfn' 
 const PGPORT='5432'
 
