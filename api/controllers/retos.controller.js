7a45caad2667f1d8df22fba6fffa50d2e0a7da3c
AlexisFAP <alexispatinoagudelo@gmail.com>
2021-10-16 01:09:12 -0500

    portada implementada

--- api/controllers/retos.controller.js
+++ api/controllers/retos.controller.js
@@ -2,8 +2,8 @@ const PostgresService = require('../services/postgres.service');
 const _pg = new PostgresService()
 
 const crearReto = async (reto) => {
-    const sql = 'INSERT INTO public.retos (id_usuario, id_tipo_reto, dependencia, problema, antecedentes, interesados, impacto_esperado,  premio, fecha) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);'
-    const datos = [reto.id_usuario, reto.id_tipo_reto, reto.dependencia, reto.problema, reto.antecedentes, reto.interesados, reto.impacto_esperado, reto.premio, reto.fecha]
+    const sql = 'INSERT INTO public.retos (id_usuario, id_tipo_reto, dependencia, problema, antecedentes, interesados, impacto_esperado,  premio, fecha, portada) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9,$10);'
+    const datos = [reto.id_usuario, reto.id_tipo_reto, reto.dependencia, reto.problema, reto.antecedentes, reto.interesados, reto.impacto_esperado, reto.premio, reto.fecha, reto.portada]
     return await _pg.ejecutarQuery(sql, datos)
 }
 
